2b797e9b135dc042bb7915f61c647296efec39a9
daniliptak <daniliptak@gmail.com>
2016-03-18 23:56:09 +0100

    develop my questionnaire
    i would like to us my recommendation modul with questions. So i started
    to trying how it's going to work, and think about a nice outfit in css
    :)

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 if (confirm("Vállalod az idoutazassal jaro felelosseget?")===true) {} else {
 	window.location.href = 'http://www.greenfoxacademy.com';
     }
-function myFunction() {
+	
+	
+//kérdésekre válaszolva adjon tanácsot//
+function myFunction() 
+{
+if (prompt("Mult vagy jövő?") === "múlt")
+	{
+	document.getElementById("recommendation").innerHTML = "Western kor";
+	}
+	else {document.getElementById("recommendation").innerHTML = "Új bolygók benépesítése 2100";}
+	
+	
+	
     document.getElementById("recommendation").innerHTML = "Western";	
 }
